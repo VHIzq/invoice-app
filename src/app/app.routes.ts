6f0f4a6ed8f5564eeb2f2663a3465a93d57e8265
vhmi <vhmizq@gmail.com>
2024-07-12 22:17:58 -0600

    routng is working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,17 @@
 import { Routes } from '@angular/router';
 import { NewInvoiceFormComponent } from './components/new-invoice-form/new-invoice-form.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: AppComponent
+    component: HomeComponent,
+    title: 'Invoices',
   },
   {
     path: 'create-invoice',
     component: NewInvoiceFormComponent,
     title: 'Create Invoice',
   },
- 
 ];
