decfbbea6fc8ee0c356b4d41379a60ddaacb63a0
vhmi <vhmizq@gmail.com>
2024-07-12 21:28:54 -0600

    move main components to home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { SidenavComponent } from './shared/sidenav/sidenav.component';
 import { CommonModule } from '@angular/common';
 import { HeadbarComponent } from './shared/headbar/headbar.component';
-import { FilterComponent } from './components/filter/filter.component';
-import { NewInvoiceComponent } from './components/new-invoice/new-invoice.component';
-import { NoInvoicesComponent } from './components/no-invoices/no-invoices.component';
-import { InvoiceComponent } from './components/invoice/invoice.component';
-import { InvoiceService } from './services/invoice.service';
-import { InvoiceModel } from './components/invoice/invoice.model';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
@@ -18,28 +13,11 @@ import { InvoiceModel } from './components/invoice/invoice.model';
     RouterOutlet,
     SidenavComponent,
     HeadbarComponent,
-    FilterComponent,
-    NewInvoiceComponent,
-    NoInvoicesComponent,
-    InvoiceComponent,
+    HomeComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'invoice-app';
-  mockData!: Array<InvoiceModel>;
-
-  constructor(private invoiceService: InvoiceService) {}
-  ngOnInit(): void {
-    this.setupMockData();
-  }
-
-  setupMockData() {
-    this.invoiceService
-      .getMockData()
-      .subscribe((invoices: Array<InvoiceModel>) => {
-        this.mockData = invoices;
-      });
-  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FilterComponent } from '../filter/filter.component';
+import { NewInvoiceComponent } from '../new-invoice/new-invoice.component';
+import { NoInvoicesComponent } from '../no-invoices/no-invoices.component';
+import { InvoiceComponent } from '../invoice/invoice.component';
+import { InvoiceModel } from '../invoice/invoice.model';
+import { InvoiceService } from '../../services/invoice.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    FilterComponent,
+    NewInvoiceComponent,
+    NoInvoicesComponent,
+    InvoiceComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  mockData!: Array<InvoiceModel>;
 
+  constructor(private invoiceService: InvoiceService) {}
+  ngOnInit(): void {
+    this.setupMockData();
+  }
+
+  setupMockData() {
+    this.invoiceService
+      .getMockData()
+      .subscribe((invoices: Array<InvoiceModel>) => {
+        this.mockData = invoices;
+      });
+  }
 }
