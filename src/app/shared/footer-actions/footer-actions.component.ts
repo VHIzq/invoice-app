833640e67c30f4e016419df20fa1878ee7f2aefd
VHMI <vhmizq.com>
2024-07-24 19:07:02 -0600

    add setup initial buttons

--- src/app/shared/footer-actions/footer-actions.component.ts
+++ src/app/shared/footer-actions/footer-actions.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { PaymentActions } from './footer-actions.model';
 
 @Component({
   selector: 'app-footer-actions',
   standalone: true,
   imports: [],
   templateUrl: './footer-actions.component.html',
-  styleUrl: './footer-actions.component.scss'
+  styleUrl: './footer-actions.component.scss',
 })
-export class FooterActionsComponent {
+export class FooterActionsComponent implements OnInit {
+  @Input()
+  paymentActions?: PaymentActions;
 
+  ngOnInit(): void {
+    this.setupInitialActions();
+  }
+
+  setupInitialActions() {
+    this.paymentActions ??= { isDiscard: true, isDraft: true, isSend: true };
+  }
 }
---
+++ src/app/shared/footer-actions/footer-actions.model.ts
@@ -0,0 +1,5 @@
+export interface PaymentActions {
+    isDiscard?: boolean;
+    isDraft?: boolean;
+    isSend?: boolean;
+}
