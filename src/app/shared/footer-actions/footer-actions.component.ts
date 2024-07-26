05da48ec687dac1641528484d2b09e0523194f44
vhmi <vhmizq@gmail.com>
2024-07-25 20:59:17 -0600

    config buttons for views with routing

--- src/app/shared/footer-actions/footer-actions.component.ts
+++ src/app/shared/footer-actions/footer-actions.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PaymentActions } from './footer-actions.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-footer-actions',
@@ -12,14 +13,18 @@ export class FooterActionsComponent implements OnInit {
   @Input()
   paymentActions?: PaymentActions;
 
+  constructor(private activatedRoute: ActivatedRoute) {}
+
   ngOnInit(): void {
     this.setupFormActions();
   }
 
   setupFormActions() {
-    if (false) {
+    const mode = this.activatedRoute.snapshot.data['mode'];
+    if (mode === 'new') {
       this.paymentActions ??= { isDiscard: true, isDraft: true, isSend: true };
-    } else {
+    } 
+    if (mode === 'edit') {
       this.paymentActions ??= { isCancel: true, isChange: true };
     }
   }
