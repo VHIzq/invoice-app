1093521835c68bbc8a7dc154f8a4ecd2f2b524e1
VHMI <vhmizq.com>
2024-07-29 20:10:46 -0600

    add modal

--- src/app/components/status-invoice/status-invoice.component.ts
+++ src/app/components/status-invoice/status-invoice.component.ts
@@ -1,25 +1,33 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { InvoiceModel } from '../invoice/invoice.model';
+import { Component, inject } from '@angular/core';
 import { BackAnchorComponent } from '../../shared/back-anchor/back-anchor.component';
 import { StatusBoxComponent } from '../../shared/status-box/status-box.component';
-import { FooterActionsComponent } from "../../shared/footer-actions/footer-actions.component";
+import { FooterActionsComponent } from '../../shared/footer-actions/footer-actions.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-status-invoice',
   standalone: true,
   imports: [
+    /* Angular Modules */
     CommonModule,
+    MatDialogModule,
+    /* Custom components */
     BackAnchorComponent,
     StatusBoxComponent,
-    CommonModule,
-    FooterActionsComponent
-],
+    FooterActionsComponent,
+  ],
   templateUrl: './status-invoice.component.html',
   styleUrl: './status-invoice.component.scss',
 })
 export class StatusInvoiceComponent {
+  constructor(private dialog: MatDialog) {}
+
   deleteInvoice() {
     console.log('invoice deleted');
+  }
+
+  openDialog() {
+    const dialogRed = this.dialog.open();
   }
 }
