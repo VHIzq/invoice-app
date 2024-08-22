f20aa267e3906e4973a87c78e9287f3a3bf41651
vhmizq <vhmizq@gmail.com>
2024-08-22 16:54:49 +0000

    add firebase auth credential

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { FormService } from '../../services/form.service';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -17,19 +18,27 @@ import { RouterModule } from '@angular/router';
     MatButtonModule,
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnInit {
   url = 'sign-up';
   formLogin!: FormGroup;
 
-  constructor(private formService: FormService) {}
+  constructor(private formService: FormService, firebaseService: AuthService) {}
   ngOnInit(): void {
     this.setupLoginForm();
   }
 
   handleLogin() {
     console.log('login user');
+  }
+
+  async login() {
+    console.log(this.formLogin.value);
+    return;
+    if (this.formLogin.valid) {
+      
+    }
   }
 
   private setupLoginForm() {
