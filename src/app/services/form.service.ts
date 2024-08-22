d0be0923d20ad5517eba55d5af352125e765b587
vhmi <vhmizq@gmail.com>
2024-08-21 22:56:19 -0600

    login updates

--- src/app/services/form.service.ts
+++ src/app/services/form.service.ts
@@ -2,23 +2,22 @@ import { Injectable } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FormService {
-
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   createForm(): FormGroup {
     return this.fb.group({
       cityFrom: ['', Validators.required],
-      streetAddressFrom :['', Validators.required],
-      postCodeFrom :['', Validators.required],
+      streetAddressFrom: ['', Validators.required],
+      postCodeFrom: ['', Validators.required],
       countryFrom: ['', Validators.required],
       clientNameTo: ['', Validators.required],
       clientEmailTo: ['', Validators.required],
       streetAddressTo: ['', Validators.required],
       cityTo: ['', Validators.required],
-      postCodeTo :['', Validators.required],
+      postCodeTo: ['', Validators.required],
       countryTo: ['', Validators.required],
       invoiceDate: ['', Validators.required],
       paymentTerms: ['', Validators.required],
@@ -28,13 +27,20 @@ export class FormService {
       price: ['', Validators.required],
       total: ['', Validators.required],
       itemId: ['', Validators.required],
-    })
+    });
   }
 
   createLoginForm() {
     return this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]]
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(20),
+        ],
+      ],
     });
   }
 
@@ -44,10 +50,30 @@ export class FormService {
       firstLastName: ['', Validators.required],
       secondLastName: ['', Validators.required],
       birthdate: ['', [Validators.required]],
-      rfc: ['', Validators.required],
+      rfc: [
+        '',
+        Validators.required,
+        Validators.maxLength(13),
+        Validators.minLength(13),
+      ],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]],
-      confirmPassword: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]] 
-    })    
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(20),
+        ],
+      ],
+      confirmPassword: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(20),
+        ],
+      ],
+    });
   }
 }
+//TODO: validate confirm matching passwords
