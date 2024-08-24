a3738a374ab4e37393833afe936c9271fa82ac7a
VHMI <vhmizq.com>
2024-08-24 00:49:32 -0600

    add guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { InvoiceFormComponent } from './components/invoice-form/invoice-form.com
 import { StatusInvoiceComponent } from './components/status-invoice/status-invoice.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -19,6 +20,7 @@ export const routes: Routes = [
     path: 'home',
     component: HomeComponent,
     title: 'Invoices',
+    canActivate: [authGuard]
   },
   {
     path: 'create-invoice',
