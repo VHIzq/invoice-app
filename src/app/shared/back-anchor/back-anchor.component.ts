6004b650d5e01e10072121118316c6f6dd6d8555
VHMI <vhmizq.com>
2024-08-29 14:31:22 -0600

    custom back anchor

--- src/app/shared/back-anchor/back-anchor.component.ts
+++ src/app/shared/back-anchor/back-anchor.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Config } from './back-anchor.model';
 
 @Component({
   selector: 'app-back-anchor',
   standalone: true,
   imports: [],
   templateUrl: './back-anchor.component.html',
-  styleUrl: './back-anchor.component.scss'
+  styleUrl: './back-anchor.component.scss',
 })
 export class BackAnchorComponent {
+  @Input()
+  config!: Config;
 
+  private router = inject(Router);
+
+  handleNavigation() {
+    this.router.navigate([this.config.url]);
+  }
 }
---
+++ src/app/shared/back-anchor/back-anchor.model.ts
@@ -0,0 +1,5 @@
+export interface Config {
+    url: string;
+    name: string;
+    hasIcon: boolean;
+}
