dd16f86e664a27c9991422919db1af0645b66a6e
vhmi <vhmizq@gmail.com>
2024-08-30 17:14:43 -0600

    add headbar menu

--- src/app/shared/headbar/headbar.component.ts
+++ src/app/shared/headbar/headbar.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MenuItem } from './headbar.component.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-headbar',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatMenuModule, CommonModule],
   templateUrl: './headbar.component.html',
   styleUrl: './headbar.component.scss',
 })
-export class HeadbarComponent {}
+export class HeadbarComponent {
+   logoutSession() {
+    console.log('logout session');
+  }
+
+  goMyProfile() {
+    console.log(' go to my settings');
+  }
+}
